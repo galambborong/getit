2477f7ed12e31021a6f19af7ffe7467658cf74bd
Peter Keenan <keenan@posteo.net>
2021-04-07 11:22:00 +0100

    Minor change to Router and add utils dir

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Router>
         <ArticlesList path="/" />
         <ArticlesList path="/articles" />
+        <ArticlesList path="/:filter/articles" />
         <TopicsList path="/topics" />
         <UsersList path="/users" />
       </Router>
