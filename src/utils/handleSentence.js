a2f8ccf6a482b188febd6f5818a146d4db29fce8
Peter Keenan <keenan@posteo.net>
2021-04-13 11:50:17 +0100

    Tidy handleSentence function

--- src/utils/handleSentence.js
+++ src/utils/handleSentence.js
@@ -2,6 +2,5 @@ export const handleSentence = (bodyStr) => {
   const sentencesRegex = /^(.*?)[.?!]\s/g;
   const sentences = bodyStr.match(sentencesRegex);
   const rawSentence = sentences === null ? bodyStr : sentences[0];
-  const sentence = rawSentence.slice(0, -2);
-  return sentence;
+    return rawSentence.slice(0, -2);
 };
