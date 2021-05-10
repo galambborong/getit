c8ea202f0b4ce56be6e659de2e6e006454ae2fd9
Peter Keenan <keenan@posteo.net>
2021-05-10 17:00:08 +0100

    Change ArticlesList from class component to functional with hooks

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Router } from '@reach/router';
 import Header from './components/Header';
 import Nav from './components/Nav';
-import ArticlesList from './components/ArticlesList';
+import { ArticlesList } from './components/ArticlesList';
 import TopicsList from './components/TopicsList';
 import UsersList from './components/UsersList';
 import Article from './components/Article';
--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from '../components/Loading';
 import { fetchArticles } from '../utils/api';
 import ArticleCard from './ArticleCard';
@@ -6,123 +6,92 @@ import Paginate from './Paginate';
 import SortList from './SortList';
 import Error from './Error';
 
-class ArticlesList extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    articles: [],
-    sort_by: 'votes',
-    page: 1,
-    limit: 10,
-    totalArticle: 0
+export const ArticlesList = (props) => {
+  console.log(props);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [articles, setArticles] = useState([]);
+  const [sort_by, setSortBy] = useState('votes');
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [totalArticle, setTotalArticle] = useState(0);
+
+  const sortListOrder = (event) => {
+    setSortBy(event);
   };
 
-  sortListOrder = (event) => {
-    this.setState({ sort_by: event });
+  const changePage = (increment) => {
+    setPage(page + increment);
+    setLoading(true);
   };
 
-  changePage = (increment) => {
-    this.setState((currState) => {
-      return {
-        page: currState.page + increment,
-        loading: true
-      };
-    });
-  };
-
-  componentDidMount() {
-    const { topic, username } = this.props;
-    const { sort_by, page, limit } = this.state;
+  useEffect(() => {
+    const { topic, username } = props;
     fetchArticles(topic, username, sort_by, page, limit)
       .then(({ articles, total_count }) => {
-        this.setState({
-          articles,
-          error: false,
-          loading: false,
-          totalArticle: total_count
-        });
+        setArticles(articles);
+        setError(false);
+        setLoading(false);
+        setTotalArticle(total_count);
       })
       .catch((error) => {
         console.dir(error);
-        this.setState({ error, loading: false });
+        setError(error);
+        setLoading(false);
       });
+  }, [sort_by, page, limit, props]);
+
+  const { topic, username, path } = props;
+
+  const randomArticles = [];
+  let counter = 3;
+
+  while (counter > 0) {
+    randomArticles.push(articles[Math.floor(Math.random() * articles.length)]);
+    counter--;
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    const { topic, username } = this.props;
-    const { sort_by, page, limit } = this.state;
-    if (prevState.sort_by !== sort_by || prevState.page !== page) {
-      fetchArticles(topic, username, sort_by, page, limit)
-        .then(({ articles, total_count }) => {
-          this.setState({ articles, total_count, loading: false });
-        })
-        .catch((error) => {
-          console.dir(error);
-          this.setState({ error, loading: false });
-        });
-    }
-  }
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
 
-  render() {
-    const { loading, articles, page, totalArticle, limit, error } = this.state;
-    const { topic, username, path } = this.props;
-
-    const randomArticles = [];
-    let counter = 3;
-
-    while (counter > 0) {
-      randomArticles.push(
-        articles[Math.floor(Math.random() * articles.length)]
-      );
-      counter--;
-    }
-
-    if (loading) return <Loading />;
-    if (error) return <Error error={error} />;
-
-    if (!loading && path === '/') {
-      return (
-        <main className="articles" data-testid="articles">
-          <h2 className="articles__header">
-            Some reading to get you started...
-          </h2>
-          <section className="articles-container">
-            {randomArticles.map((article) => {
-              return <ArticleCard article={article} key={article.article_id} />;
-            })}
-          </section>
-        </main>
-      );
-    }
-
+  if (!loading && path === '/') {
     return (
       <main className="articles" data-testid="articles">
-        {topic && <h2 className="articles__header">{topic} articles</h2>}
-        {username && (
-          <h2 className="articles__header articles__header--user">
-            @{username}'s articles
-          </h2>
-        )}
-        {!topic && !username && (
-          <h2 className="articles__header">All articles</h2>
-        )}
-        <div className="controls">
-          <SortList sortListOrder={this.sortListOrder} />
-        </div>
+        <h2 className="articles__header">Some reading to get you started...</h2>
         <section className="articles-container">
-          {articles.map((article) => {
+          {randomArticles.map((article) => {
             return <ArticleCard article={article} key={article.article_id} />;
           })}
         </section>
-        <Paginate
-          changePage={this.changePage}
-          page={page}
-          limit={limit}
-          totalArticle={totalArticle}
-        />
       </main>
     );
   }
-}
 
-export default ArticlesList;
+  return (
+    <main className="articles" data-testid="articles">
+      {topic && <h2 className="articles__header">{topic} articles</h2>}
+      {username && (
+        <h2 className="articles__header articles__header--user">
+          @{username}'s articles
+        </h2>
+      )}
+      {!topic && !username && (
+        <h2 className="articles__header">All articles</h2>
+      )}
+      <div className="controls">
+        <SortList sortListOrder={sortListOrder} />
+      </div>
+      <section className="articles-container">
+        {articles.map((article) => {
+          return <ArticleCard article={article} key={article.article_id} />;
+        })}
+      </section>
+      <Paginate
+        changePage={changePage}
+        page={page}
+        limit={limit}
+        totalArticle={totalArticle}
+      />
+    </main>
+  );
+};
