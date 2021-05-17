66660575d9d3c70f58c798418f13eb52f236f2d1
Peter Keenan <keenan@posteo.net>
2021-05-17 22:28:26 +0100

    Convert TopicsList to functional component with hooks

--- src/components/TopicsList.jsx
+++ src/components/TopicsList.jsx
@@ -1,47 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchTopicsList } from '../utils/api';
 import Loading from './Loading';
 import Error from './Error';
 import TopicCard from './TopicCard';
 
-class TopicsList extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    topics: [],
-  };
+export const TopicsList = ({}) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [topics, setTopics] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetchTopicsList()
       .then((res) => {
-        this.setState({ loading: false, error: false, topics: res });
+        setLoading(false);
+        setError(false);
+        setTopics(res);
       })
       .catch((error) => {
         console.dir(error);
-        this.setState({ loading: false, error });
+        setError(error);
+        setLoading(false);
       });
-  }
+  }, []);
 
-  render() {
-    const { loading, topics, error } = this.state;
-    if (loading) return <Loading />;
-    if (error) return <Error error={error} />;
-    return (
-      <main className="topics">
-        <h2 className="topics__header">Browse by topic...</h2>
-        <ul className="topics__list">
-          {topics.map((topic) => {
-            const { slug } = topic;
-            return (
-              <li className="topics__link" key={slug}>
-                <TopicCard topic={topic} />
-              </li>
-            );
-          })}
-        </ul>
-      </main>
-    );
-  }
-}
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  return (
+    <main className="topics">
+      <h2 className="topics__header">Browse by topic...</h2>
+      <ul className="topics__list">
+        {topics.map((topic) => {
+          const { slug } = topic;
+          return (
+            <li className="topics__link" key={slug}>
+              <TopicCard topic={topic} />
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+};
 
 export default TopicsList;
