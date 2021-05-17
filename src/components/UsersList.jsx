e61ae4b84f8164801a31d9b86139df0eeaa2f933
Peter Keenan <keenan@posteo.net>
2021-05-17 22:39:33 +0100

    Convert UsersList to functional component with hooks

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,43 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchUsersList } from '../utils/api';
 import Error from './Error';
 import Loading from './Loading';
 import UserCard from './UserCard';
 
-class UsersList extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    users: []
-  };
+export const UsersList = ({}) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [users, setUsers] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     fetchUsersList()
       .then((users) => {
-        this.setState({ users, loading: false, error: false });
+        setUsers(users);
+        setLoading(false);
+        setError(false);
       })
       .catch((error) => {
         console.dir(error);
-        this.setState({ error: error, loading: false });
+        setError(error);
+        setLoading(false);
       });
-  }
+  }, []);
 
-  render() {
-    const { users, loading, error } = this.state;
-    if (loading) return <Loading />;
-    if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
 
-    return (
-      <main className="users">
-        <h2 className="users__header">get:it authors</h2>
-        <section className="users-container">
-          {users.map((user) => {
-            return <UserCard user={user} key={user.username} />;
-          })}
-        </section>
-      </main>
-    );
-  }
-}
+  return (
+    <main className="users">
+      <h2 className="users__header">get:it authors</h2>
+      <section className="users-container">
+        {users.map((user) => {
+          return <UserCard user={user} key={user.username} />;
+        })}
+      </section>
+    </main>
+  );
+};
 
 export default UsersList;
