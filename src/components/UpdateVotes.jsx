ca6961d0a3b0d7420ebeafeb9e94c4df3235e50a
Peter Keenan <keenan@posteo.net>
2021-05-17 22:36:04 +0100

    Convert UpdateVotes to functional component with hooks

--- src/components/UpdateVotes.jsx
+++ src/components/UpdateVotes.jsx
@@ -1,62 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { patchVote } from '../utils/api';
 
-class UpdateVotes extends React.Component {
-  state = {
-    votesChange: 0,
-    hasVoted: false,
-  };
+export const UpdateVotes = ({ article_id, votes, comment_id }) => {
+  const [votesChange, setVotesChange] = useState(0);
+  const [hasVoted, setHasVoted] = useState(false);
 
-  handleClick = (articleId, increment, commentId) => {
-    if (!this.state.hasVoted) {
-      this.setState((currentState) => {
-        return {
-          votesChange: currentState.votesChange + increment,
-          hasVoted: true,
-        };
-      });
+  const handleClick = (articleId, increment, commentId) => {
+    if (!hasVoted) {
+      setVotesChange(votesChange + increment);
+      setHasVoted(true);
       patchVote(articleId, increment, commentId).catch((err) => {
         console.dir(err);
-        this.setState((currState) => {
-          return {
-            votesChange: currState.votesChange - increment,
-            hasVoted: false,
-          };
-        });
+        setVotesChange(votesChange - increment);
+        setHasVoted(false);
       });
     }
   };
 
-  render() {
-    const { article_id, votes, comment_id } = this.props;
-    const { votesChange, hasVoted } = this.state;
+  let mainClassName;
 
-    let mainClassName;
+  !comment_id
+    ? (mainClassName = 'vote')
+    : (mainClassName = 'comment-card__vote');
 
-    !comment_id
-      ? (mainClassName = 'vote')
-      : (mainClassName = 'comment-card__vote');
-
-    return (
-      <div className={mainClassName}>
-        <button
-          className="vote__btn"
-          disabled={hasVoted}
-          onClick={() => this.handleClick(article_id, 1, comment_id)}
-        >
-          <i className="fas fa-thumbs-up" />
-        </button>
-        {votes + votesChange}
-        <button
-          className="vote__btn"
-          disabled={hasVoted}
-          onClick={() => this.handleClick(article_id, -1, comment_id)}
-        >
-          <i className="fas fa-thumbs-down" />
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={mainClassName}>
+      <button
+        className="vote__btn"
+        disabled={hasVoted}
+        onClick={() => handleClick(article_id, 1, comment_id)}
+      >
+        <i className="fas fa-thumbs-up" />
+      </button>
+      {votes + votesChange}
+      <button
+        className="vote__btn"
+        disabled={hasVoted}
+        onClick={() => handleClick(article_id, -1, comment_id)}
+      >
+        <i className="fas fa-thumbs-down" />
+      </button>
+    </div>
+  );
+};
 
 export default UpdateVotes;
