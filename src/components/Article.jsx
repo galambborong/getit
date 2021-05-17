4b0550f631d41642bfff28b42870bdb7125e46b3
Peter Keenan <keenan@posteo.net>
2021-05-17 22:00:22 +0100

    Convert Article to functional component with hooks

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchArticleById } from '../utils/api';
 import { Link } from '@reach/router';
 import Loading from './Loading';
@@ -6,70 +6,56 @@ import UpdateVotes from './UpdateVotes';
 import AddComment from './AddComment';
 import Error from './Error';
 
-class Article extends React.Component {
-  state = {
-    loading: true,
-    error: null,
-    article: {},
-    newComment: null
+export const Article = ({ article_id }) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [article, setArticle] = useState({});
+  const [newComment, setNewComment] = useState(null);
+
+  const confirmComment = () => {
+    setNewComment(true);
   };
 
-  confirmComment = () => {
-    this.setState({ newComment: true });
-  };
-
-  componentDidMount() {
-    fetchArticleById(this.props.article_id)
+  useEffect(() => {
+    fetchArticleById(article_id)
       .then((article) => {
-        this.setState({ article, loading: false, error: false });
+        setArticle(article);
+        setError(false);
+        setLoading(false);
       })
       .catch((err) => {
         console.dir(err);
-        this.setState({ error: err, loading: false });
+        setError(err);
+        setLoading(false);
       });
-  }
+  }, [article_id]);
 
-  render() {
-    const { loading, error, article } = this.state;
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
 
-    if (loading) return <Loading />;
-    if (error) return <Error error={error} />;
+  const { author, body, created_at, title, topic, votes } = article;
 
-    const {
-      author,
-      body,
-      created_at,
-      title,
-      topic,
-      votes,
-      article_id
-    } = article;
+  const date = new Date(created_at);
 
-    const date = new Date(created_at);
-
-    return (
-      <div className="article-wrapper">
-        <main className="article">
-          <h2 className="article__header">{title}</h2>
-          <h3 className="article__author">@{author}</h3>
-          <p className="article__body">{body}</p>
-          <p className="article__date">{date.toDateString()}</p>
-          <UpdateVotes article_id={article_id} votes={votes} />
-          <p className="article__topic">#{topic}</p>
-          <Link
-            to={`/articles/${article_id}/comments`}
-            className="article__comments"
-          >
-            See comments
-          </Link>
-        </main>
-        <AddComment
-          article_id={article_id}
-          confirmComment={this.confirmComment}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="article-wrapper">
+      <main className="article">
+        <h2 className="article__header">{title}</h2>
+        <h3 className="article__author">@{author}</h3>
+        <p className="article__body">{body}</p>
+        <p className="article__date">{date.toDateString()}</p>
+        <UpdateVotes article_id={article_id} votes={votes} />
+        <p className="article__topic">#{topic}</p>
+        <Link
+          to={`/articles/${article_id}/comments`}
+          className="article__comments"
+        >
+          See comments
+        </Link>
+      </main>
+      <AddComment article_id={article_id} confirmComment={confirmComment} />
+    </div>
+  );
+};
 
 export default Article;
